ef921e8b7756a289cefd7a95391bb1453df61a2b
Blaine Bublitz <blaine@iceddev.com>
2015-03-23 19:04:20 -0700

    split webpack config into its own file and add webpack-dev-server command

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const path = require('path');
-
 const del = require('del');
 const gulp = require('gulp');
 const chalk = require('chalk');
@@ -9,7 +7,7 @@ const zip = require('gulp-zip');
 const gutil = require('gulp-util');
 const webpack = require('webpack');
 
-const shouldWatch = (process.argv.indexOf('--watch') !== -1);
+const webpackConfig = require('./webpack.config');
 
 const files = {
   release: [
@@ -20,61 +18,6 @@ const files = {
     '_locales/**',
     'icons/**'
   ]
-};
-
-const webpackConfig = {
-  entry: './client.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  module: {
-    noParse: [
-      /pbasic/i
-    ],
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: [
-          /node_modules/,
-          /pbasic/i
-        ],
-        loaders: [
-          'babel-loader?optional=runtime'
-        ]
-      }
-    ]
-  },
-  resolveLoader: {
-    // this is a workaround for loaders being applied
-    // to linked modules
-    root: path.join(__dirname, 'node_modules')
-  },
-  resolve: {
-    // this is a workaround for aliasing a top level dependency
-    // inside a symlinked subdependency
-    root: path.join(__dirname, 'node_modules'),
-    alias: {
-      // replacing `fs` with a browser-compatible version
-      fs: 'browserify-fs',
-      memdown: 'level-js',
-      serialport: 'browser-serialport'
-    }
-  },
-  bail: true,
-  watch: shouldWatch
 };
 
 function js(cb){
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var path = require('path');
+
+var shouldWatch = (process.argv.indexOf('--watch') !== -1);
+
+module.exports = {
+  entry: './client.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    noParse: [
+      /pbasic/i
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /pbasic/i
+        ],
+        loaders: [
+          'babel-loader?optional=runtime'
+        ]
+      }
+    ]
+  },
+  resolveLoader: {
+    // this is a workaround for loaders being applied
+    // to linked modules
+    root: path.join(__dirname, 'node_modules')
+  },
+  resolve: {
+    // this is a workaround for aliasing a top level dependency
+    // inside a symlinked subdependency
+    root: path.join(__dirname, 'node_modules'),
+    alias: {
+      // replacing `fs` with a browser-compatible version
+      fs: 'browserify-fs',
+      memdown: 'level-js',
+      serialport: 'browser-serialport'
+    }
+  },
+  bail: true,
+  watch: shouldWatch
+};
