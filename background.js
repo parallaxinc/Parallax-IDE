e3382c9995c501076c4da8f05554c417abb71d52
Matthew Shepard <mattshepard@gmail.com>
2015-06-02 15:47:12 -0700

    Close serial ports from background page when window is closed to prevent port leakage.

--- background.js
+++ background.js
@@ -1,18 +1,32 @@
 'use strict';
 
+function closeSerialPorts(){
+  chrome.serial.getConnections(function(connections){
+    connections.forEach(function(connection){
+      chrome.serial.disconnect(connection.connectionId, function(){});
+    });
+  });
+}
+
 // Listens for the app launching then creates the window
 chrome.app.runtime.onLaunched.addListener(function() {
   var width = 1000;
   var height = 500;
 
-  chrome.app.window.create('index.html', {
+  var windowOpts = {
     id: 'main',
     state: 'maximized',
     bounds: {
       width: width,
       height: height,
       left: Math.round((screen.availWidth - width) / 2),
-      top: Math.round((screen.availHeight - height)/2)
+      top: Math.round((screen.availHeight - height) / 2)
     }
+  };
+
+  chrome.app.window.create('index.html', windowOpts, function(win){
+    win.onClosed.addListener(function(){
+      closeSerialPorts();
+    });
   });
 });
