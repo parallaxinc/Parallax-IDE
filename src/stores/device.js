85d751abe170de295b3f84feaeb82ca9fb0f6e0b
drquinn <blong123@gmail.com>
2015-07-01 16:49:47 -0700

    added device check logic for auto download

--- src/stores/device.js
+++ src/stores/device.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const alt = require('../alt');
+const _ = require('lodash');
 
 const { rx, tx } = require('../actions/transmission');
 const { showDownload } = require('../actions/overlay');
 const { clearOutput, output } = require('../actions/console');
 const { download, reloadDevices, updateSelected } = require('../actions/device');
+
+const styles = require('../../plugins/sidebar/styles');
 
 class DeviceStore {
   constructor() {
@@ -19,7 +22,7 @@ class DeviceStore {
     this.state = {
       devices: [],
       devicePath: null,
-      message: 'Device not found',
+      message: null,
       searching: true,
       selectedDevice: null,
       progress: 0
@@ -51,9 +54,8 @@ class DeviceStore {
     board.on('progress', updateProgress.bind(this));
     board.on('progress', tx.bind(this));
 
-    board.compile(source)
+    board.bootload(selectedDevice)
       .tap(() => clearOutput())
-      .then((memory) => board.bootload(memory))
       .then(() => board.on('terminal', output))
       .then(() => board.on('terminal', rx))
       .tap(() => toast.clear())
@@ -68,18 +70,68 @@ class DeviceStore {
 
   onReloadDevices(){
 
-    const { scanBoards } = this.getInstance();
+    const { scanBoards, workspace } = this.getInstance();
+    const source = workspace.current.deref();
 
     const scanOpts = {
       reject: [
         /Bluetooth-Incoming-Port/,
         /Bluetooth-Modem/,
         /dev\/cu\./
-      ]
+      ],
+      source: source
     };
     this.setState({ devicePath: null, searching: true });
     scanBoards(scanOpts)
-      .then((devices) => this.setState({ devices: devices, searching: false }));
+    .then((devices) => this.setState({ devices: devices, searching: false }))
+    .then(() => this._checkDevices());
+
+  }
+
+  _checkDevices(){
+    const { devices } = this.state;
+    const noModulesFoundMessage = 'No BASIC Stamps found.';
+    const multipleModulesFoundMessage = 'Please select which module to download to.';
+    let matchedDevices = [];
+
+    _.forEach(devices, (device) => {
+      if (device.match) {
+        matchedDevices.push(device);
+      }
+    });
+    if (matchedDevices.length === 0) {
+      console.log('devicenot found');
+      this.setState({ message: noModulesFoundMessage });
+    }
+    else if (matchedDevices.length === 1) {
+      console.log('one match found');
+      this.setState({
+        message: null,
+        selectedDevice: matchedDevices[0]
+      });
+      this.onDownload({
+        handleSuccess: this._handleSuccess.bind(this),
+        handleError: this._handleError.bind(this)
+      });
+    }
+    else {
+      console.log('multiple matches found');
+      this.setState({ message: multipleModulesFoundMessage });
+    }
+  }
+
+  _handleError(err){
+    // leaving this in for better debugging of errors
+    console.log(err);
+    const { toast } = this.getInstance();
+
+    toast.show(err.message, { style: styles.errorToast });
+  }
+
+  _handleSuccess(msg){
+    const { toast } = this.getInstance();
+
+    toast.show(msg, { style: styles.successToast, timeout: 5000 });
   }
 
   onUpdateSelected(device) {
@@ -87,7 +139,13 @@ class DeviceStore {
       devicePath: device.path,
       selectedDevice: device
     });
+
+    this.onDownload({
+      handleSuccess: this._handleSuccess.bind(this),
+      handleError: this._handleError.bind(this)
+    });
   }
+
 }
 
 DeviceStore.config = {
