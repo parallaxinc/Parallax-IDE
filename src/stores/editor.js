8ef9f6ea4a3ec8e7b332a150cb1c89081e8dd5f6
Blaine Bublitz <blaine@iceddev.com>
2015-07-01 18:46:11 -0700

    make print work

--- src/stores/editor.js
+++ src/stores/editor.js
@@ -56,7 +56,14 @@ class EditorStore {
     cm.execCommand('insertSoftTab');
   }
   onPrint() {
+    const { cm, workspace } = this.getInstance();
+
+    const { title } = document;
+    document.title = workspace.filename.deref();
+    cm.setOption('viewportMargin', Infinity);
     window.print();
+    document.title = title;
+    cm.setOption('viewportMargin', 10);
   }
 
 }
