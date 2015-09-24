af0c0a25f7702fd712c063296c15e8c7e0317a15
Matthew Shepard <mattshepard@gmail.com>
2015-09-24 15:56:19 -0700

    Add echo toggle to rx/tx bar

--- src/plugins/rxtx.js
+++ src/plugins/rxtx.js
@@ -32,6 +32,17 @@ const styles = {
     float: 'right',
     paddingTop: '3px'
   },
+  echo: {
+    display: 'inline-block',
+    paddingTop: '3px',
+    paddingLeft: '16px'
+  },
+  echoOn: {
+    color: '#000000'
+  },
+  echoOff: {
+    color: '#888888'
+  },
   rx: {
     backgroundColor: blue
   },
@@ -49,9 +60,10 @@ function rxtxBar(app, opts, done){
   let bottomBar;
   let rx;
   let tx;
+  let echoContainer, echoLabel;
 
   function onConsoleChange(){
-    const { rxtx } = consoleStore.getState();
+    const { rxtx, echo } = consoleStore.getState();
     const { flashRx, flashTx } = rxtx;
 
     if(flashRx){
@@ -65,6 +77,14 @@ function rxtxBar(app, opts, done){
     } else {
       tx.style.backgroundColor = styles.indicator.backgroundColor;
     }
+
+    if(echo){
+      applyStyles(echoContainer, styles.echoOn);
+      echoLabel.nodeValue = 'Echo On';
+    }else{
+      applyStyles(echoContainer, styles.echoOff);
+      echoLabel.nodeValue = 'Echo Off';
+    }
   }
 
   app.view('editor', function(el, cb){
@@ -72,6 +92,14 @@ function rxtxBar(app, opts, done){
     if(!bottomBar){
       bottomBar = document.createElement('div');
       applyStyles(bottomBar, styles.bar);
+
+      echoContainer = document.createElement('span');
+      applyStyles(echoContainer, styles.echo);
+      applyStyles(echoContainer, styles.echoOff);
+      bottomBar.appendChild(echoContainer);
+
+      echoLabel = document.createTextNode('Echo Off');
+      echoContainer.appendChild(echoLabel);
 
       const rxtxContainer = document.createElement('span');
       applyStyles(rxtxContainer, styles.rxtx);
